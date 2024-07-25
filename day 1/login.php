79f5238a60ff7762f657eb6823ca1afdaea7b914
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2024-07-25 16:31:01 +0300

    refactor: Improve login function and remove unnecessary code

--- day 1/login.php
+++ day 1/login.php
@@ -1,13 +1,20 @@
 <?php
 
-$email = "user@example.com";
-$password = "password123";
-
-    if ($email === $email && $password === $password) {
-        echo "Logged in";
+// create a function that does the login simulation
+function login($email, $password){
+    // check if the email and password are correct using an if statement
+    if ($email === "email@email.com" && $password === "password123") {
+        // if the email and password are correct, return "Logged in"
+        return "Logged in";
     }
      else {
-        echo "Invalid credentials";
+        // if the email and password are incorrect, return "Invalid credentials"
+        return "Invalid credentials";
     }
+}
 
-
+// call the function and pass the email and password
+echo "First test:=> ".login("XXXXXXXXXXXXXXXXXXXXXXXXXXX", "password123");
+// call the function and pass the email and password
+echo "Second test:=> ".login("elishahezekiah903@gmail.com", "password123");
+?>
